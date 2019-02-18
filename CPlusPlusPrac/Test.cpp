592a1e6eea55eb1fa579b6b57d781dc2db030423
jfocus <JFocus>
2019-02-18 18:47:52 +0800

    Hello

---
+++ CPlusPlusPrac/Test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+int main()
+{
+	int a[50] ;
+        memset(a,0,50);
+	for(int i =0;i<50;i++)
+	{
+		cout<<a[i]<<endl;
+	}
+}
