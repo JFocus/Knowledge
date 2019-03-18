ff23ec2ec1895b9f9980ddf7cf5e266ba06e4764
jfocus <JFocus>
2019-03-18 16:37:34 +0800

    2019/03/18

--- CPlusPlusPrac/Test.cpp
+++ CPlusPlusPrac/Test.cpp
@@ -4,10 +4,6 @@
 using namespace std;
 int main()
 {
-	int a[50] ;
-        memset(a,0,50);
-	for(int i =0;i<50;i++)
-	{
-		cout<<a[i]<<endl;
-	}
+	cout<<"hello world"<<endl;
+	cout<<((1<<1)|1)<<endl;
 }
